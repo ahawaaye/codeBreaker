2925b44a6c2ab8598c740b0acfb12ed41283603f
ahawaaye <ahawa004@gmail.com>
2024-09-13 15:48:27 +0200

    masterMindProject

---
+++ masterMind/src/masterMind/Mastermind2.java
@@ -0,0 +1,44 @@
+package masterMind;
+
+public class Mastermind2 {
+
+	public static void main(String[] args) 
+	{
+		int roodPin = 1; 
+		int oranjePin = 2; 
+		int geelPin = 3; 
+		int blauwPin = 4; 
+		int paarsPin = 5; 
+		int groenPin = 6; 
+		int zwartPin = 7; 
+		int witPin = 8; 
+		
+		int codeCrackerRij1Vak1; 
+		int codeCrackerRij1Vak2; 
+		int codeCrackerRij1Vak3; 
+		int codeCrackerRij1Vak4;
+		
+		int codeCrackerRij2Vak1; 
+		int codeCrackerRij2Vak2; 
+		int codeCrackerRij2Vak3; 
+		int codeCrackerRij2Vak4; 
+		
+		int codeCrackerRij3Vak1; 
+		int codeCrackerRij3Vak2; 
+		int codeCrackerRij3Vak3; 
+		int codeCrackerRij3Vak4; 
+		
+		int codeCrackerRij4Vak1;
+		int codeCrackerRij4Vak2;
+		int codeCrackerRij4Vak3;
+		int codeCrackerRij4Vak4;
+		
+		int codeCrackerRij5Vak1;
+		int codeCrackerRij5Vak2;
+		int codeCrackerRij5Vak3;
+		int codeCrackerRij5Vak4;
+		// TODO Auto-generated method stub
+
+	}
+
+}
