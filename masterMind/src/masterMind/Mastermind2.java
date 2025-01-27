b6d5d69d5b3fa6da2578fb1204d44108cc2b5deb
ahawaaye <ahawa004@gmail.com>
2025-01-27 15:59:02 +0100

    hm

--- masterMind/src/masterMind/Mastermind2.java
+++ masterMind/src/masterMind/Mastermind2.java
@@ -15,43 +15,35 @@ public class Mastermind2 {
 
 		int[] antwoordVak1 = fn.CodeGenereren();
 
-
 		int[] codeMakerRijen1 = { 9, 9, 9, 9 };
 
-		for (int poging1 = 1; poging1 <= 10; poging1++ ) {
+		for (int poging1 = 1; poging1 <= 10; poging1++) {
 			int[] codeCrackerRijvakken1 = fn.userInput();
 			System.out.println("poging" + ' ' + poging1);
 			// start corrective vak1
-			
-			String feedback = "Geen";
 
-			for (int i = 0; i < antwoordVak1.length; i++) {
-				if (antwoordVak1[i] == codeCrackerRijvakken1[i]) {
-					feedback = "wit";
-				} else {
-					for (int j = 0; j < antwoordVak1.length; j++) {
-						if (antwoordVak1[j] == codeCrackerRijvakken1[j] && i != j) {
-							feedback = "zwart";
+			String[] Feedback = fn.Feedback(antwoordVak1, codeCrackerRijvakken1);
 
-						}
-					}
-				}
+			for (String feedbackString : Feedback) {
+
+				System.out.println(feedbackString);
 			}
+
 			// alles
 
-			if (antwoordVak1[0] == codeCrackerRijvakken1[0] && antwoordVak1[1] == codeCrackerRijvakken1[1]
-					&& antwoordVak1[2] ==codeCrackerRijvakken1[2] && antwoordVak1[3] == codeCrackerRijvakken1[3]) {
+			boolean winning = fn.winning(antwoordVak1, codeCrackerRijvakken1);
 
-				System.out.println("je hebt gewonnen!!!!!!");
+			if (winning) {
 				break;
 
 			}
-			if (poging1 == 10) 
+
+			if (poging1 == 10)
 				System.out.println("helaas je hebt niet gewonnen");
-			}
-
-			// TODO Auto-generated method stub
-
 		}
-	
+
+		// TODO Auto-generated method stub
+
+	}
+
 }
--- masterMind/src/masterMind/function.java
+++ masterMind/src/masterMind/function.java
@@ -5,31 +5,79 @@ import java.util.Scanner;
 public class function {
 	public int[] CodeGenereren() {
 		int[] pins = { 1, 2, 3, 4, 5, 6 };
-		int[] antwoordVak1 = { pins[4], pins[1], pins[5], pins[0] };
+		int[] antwoordVak1 = { pins[4], pins[1], pins[5], pins[0] }; // 5 2 6 1
 
 		return antwoordVak1;
 
 	}
 
 	public int[] userInput() {
-
 		Scanner sc = new Scanner(System.in);
-		int[] codeCrackerRijvakken1 = { sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt() };
+		int[] codeCrackerRijvakken1 = new int[4];
+
+		for (int i = 0; i < 4; i++) {
+			try {
+				codeCrackerRijvakken1[i] = sc.nextInt();
+			} catch (Exception ex) {
+				System.out.println("Ongeldig, gebruik nummers!");
+				sc.next();
+				i--;
+			}
+
+		}
 
 		return codeCrackerRijvakken1;
 
 	}
 
-	public int poging() {
+	public String[] Feedback(int[] antwoordVak1, int[] codeCrackerRijvakken1) {
 
-		for (int poging1 = 1; poging1 <= 10; poging1++) {
-			System.out.println("poging" + ' ' + poging1);
+		String[] feedback = new String[4];
+		for (int i = 0; i < antwoordVak1.length; i++) {
+			feedback[i] = "fout";
+			if (antwoordVak1[i] == codeCrackerRijvakken1[i]) {
+				feedback[i] = "wit";
+			} else {
+				for (int j = 0; j < antwoordVak1.length; j++) {
+					if (antwoordVak1[i] == codeCrackerRijvakken1[j]) {
+						feedback[i] = "zwart";
+						break;
 
-			return poging1;
+					}
+				}
+
+			}
 
 		}
-		return 0;
+
+		return feedback;
 
 	}
-	
+
+	public function() {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Welkom bij Mastermind!");
+		System.out.println("Raad de geheime code van 4 cijfers. Elk cijfer is tussen 1 en 6.");
+		System.out.println("Je hebt 10 pogingen. Laten we beginnen!");
+
+		return;
+
+	}
+
+	public boolean winning(int[] antwoordVak1, int[] codeCrackerRijvakken1) {
+
+		if (antwoordVak1[0] == codeCrackerRijvakken1[0] && antwoordVak1[1] == codeCrackerRijvakken1[1]
+				&& antwoordVak1[2] == codeCrackerRijvakken1[2] && antwoordVak1[3] == codeCrackerRijvakken1[3]) {
+
+			System.out.println("Je hebt gewonnen!!!!!!");
+
+			return true;
+
+		}
+		return false;
+
+	}
+
 }
